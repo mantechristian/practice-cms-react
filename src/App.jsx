347c644d4e790ddef39181ab27f7715d6d1a5c6c
Christian Mante <cmante@fullscale.io>
2023-10-13 20:09:56 +0800

    Restrict routes for unauthenticated users

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect, useState } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import './App.css';
 import Dashboard from "./components/Dashboard";
 import Header from './components/Header';
@@ -12,25 +12,35 @@ import { decodeAccessToken } from './utils';
 function App() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   useLayoutEffect(() => {
-    const accessToken = localStorage.getItem('accessToken')?.toString();
-    const decoded = decodeAccessToken(accessToken);
-    const date = new Date(decoded.exp * 1000);
-    const isExpired = date.getTime() < Date.now();
-    setIsAuthenticated(!isExpired);
+    try {
+      const accessToken = localStorage.getItem('accessToken')?.toString();
+      const decoded = decodeAccessToken(accessToken);
+      const date = new Date(decoded.exp * 1000);
+      const isExpired = date.getTime() < Date.now();
+      setIsAuthenticated(!isExpired);
+    } catch (error) {
+      console.log('error', error);
+      setIsAuthenticated(false);
+    }
   }, []);
 
   return (
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={isAuthenticated ? <Navigate to="/dashboard" /> : <Navigate to="/login" />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/register" element={<SignUp />} />
-        <Route path="/dashboard" element={<ContactsProvider><Dashboard /></ContactsProvider>} />
+        {isAuthenticated ? (
+          <Route path="/dashboard" element={<ContactsProvider><Dashboard /></ContactsProvider>} />
+        ) : (
+          navigate('/login')
+        )}
       </Routes>
-    </BrowserRouter>
+    </>
   )
 }
 
